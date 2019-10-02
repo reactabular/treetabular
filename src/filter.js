2d38c9f047b5295b59b3bd51486667ec02f1100c
Laura Meikle <laura.s.meikle@gmail.com>
2019-10-02 14:16:45 +0100

    filter performance improvement

--- src/filter.js
+++ src/filter.js
@@ -9,16 +9,30 @@ const filterTree = ({
     throw new Error('tree.filterTree - Missing fieldName!');
   }
 
+  // build up a cache of parent ids and whether their child rows should show
+  // to reduce the amount of lookups for performance
+  const parentCache = {};
+
   return rows.filter((row, index) => {
     if (typeof row[parentField] === 'undefined' || row[parentField] === null) {
       return true;
     }
 
+    const parentVal = parentCache[row[parentField]];
+    if (parentVal === false || parentVal === true) {
+      return parentVal;
+    }
+
     const parents = getParents({ index, idField, parentField })(rows);
 
-    return parents.filter(
+    // if all the rows parents are showing then we know that the row should should
+    const rowShouldShow = parents.filter(
       parent => parent[fieldName]
     ).length === parents.length;
+
+    parentCache[row[parentField]] = rowShouldShow;
+
+    return rowShouldShow;
   });
 };
 
